3431f5db2e94470725b0b6518173f45e8d01becd
AruSeito <AruSeito@gmail.com>
2021-02-12 10:42:46 +0800

    feat: ✨ 使用font-awesome图标

--- src/components/PricesList.tsx
+++ src/components/PricesList.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
 interface ICategory {
   id: string;
   name: string;
   type: "income" | "outcome";
+  iconName: string;
 }
 
 export interface IItem {
@@ -29,29 +29,26 @@ const PricesList = (props: IProps) => {
           key={item.id}
         >
           <span className="col-1 badge badge-primary">
-            {item.category.name}
+            <i className={`fa ${item.category.iconName}`}></i>
           </span>
           <span className="col-5">{item.title}</span>
           <span className="col-2 font-weight-bold">{`${
             item.category.type === "income" ? "+" : "-"
-          }${item.price}`}</span>
+          }${item.price}元`}</span>
           <span className="col-2">{item.date}</span>
-          <button
-            className="col-1 btn btn-primary"
+
+          <i
+            className="fa fa-pencil"
             onClick={() => {
               handleChangeItem(item);
             }}
-          >
-            编辑
-          </button>
-          <button
-            className="col-1 btn btn-danger"
+          />
+          <i
+            className="fa fa-trash"
             onClick={() => {
               handleDelItem(item);
             }}
-          >
-            删除
-          </button>
+          />
         </li>
       ))}
     </ul>
