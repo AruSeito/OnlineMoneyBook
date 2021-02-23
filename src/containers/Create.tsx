6820c2a027638e653d42ecc95cb915f960889611
AruSeito <AruSeito@gmail.com>
2021-02-23 22:21:38 +0800

    feat: ✨ 将组件拼装成页面

--- src/containers/Create.tsx
+++ src/containers/Create.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import CategorySelector, { category } from "../components/CategorySelector";
+import PriceForm from "../components/PriceForm";
+import { Tab, Tabs } from "../components/Tabs";
 
 interface IState {
   type: "income" | "outcome";
 }
-
+const testCategories: Array<category> = [
+  {
+    id: "1",
+    name: "旅行",
+    type: "outcome",
+    inconName: "fa-plane",
+  },
+  {
+    id: "2",
+    name: "理财",
+    type: "income",
+    inconName: "fa-bank",
+  },
+  {
+    id: "3",
+    name: "贷款",
+    type: "income",
+    inconName: "fa-money",
+  },
+];
 export default class Create extends React.Component<any, IState> {
   constructor(props: any) {
     super(props);
@@ -12,6 +34,24 @@ export default class Create extends React.Component<any, IState> {
     };
   }
   render() {
-    return <div>Create Page{this.props.match.params.id}</div>;
+    const categories = testCategories.filter((category) => {
+      return category.type === this.state.type;
+    });
+    return (
+      <div
+        className="create-page py-3 px-3 rounded mt-3"
+        style={{ backgroundColor: "#fff" }}
+      >
+        <Tabs activeIndex={0} onTabChange={() => {}}>
+          <Tab>支出</Tab>
+          <Tab>收入</Tab>
+        </Tabs>
+        <CategorySelector
+          categories={categories}
+          handleSelectCategory={() => {}}
+        />
+        <PriceForm handleCancelSubmit={() => {}} handleFormSubmit={() => {}} />
+      </div>
+    );
   }
 }
