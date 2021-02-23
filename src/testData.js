cbcc191d2c90feb7c03c2f9e00806e181cc543cb
AruSeito <AruSeito@gmail.com>
2021-02-23 22:22:07 +0800

    test: ✅ 添加测试数据

---
+++ src/testData.js
@@ -0,0 +1,101 @@
+export const testCategories = [
+  {
+    "name": "旅行",
+    "iconName": "fa-plane",
+    "id": "1",
+    "type": "outcome"
+  },
+  {
+    "name": "餐饮",
+    "iconName": "fa-cutlery",
+    "id": "2",
+    "type": "outcome"
+  },
+  {
+    "name": "购物",
+    "iconName": "fa-shopping-basket",
+    "id": "3",
+    "type": "outcome"
+  },
+  {
+    "name": "数码",
+    "iconName": "fa-mobile-phone",
+    "id": "4",
+    "type": "outcome"
+  },
+  {
+    "name": "工资",
+    "iconName": "fa-credit-card",
+    "id": "10",
+    "type": "income"
+  },
+  {
+    "name": "兼职",
+    "iconName": "fa-money",
+    "id": "11",
+    "type": "income"
+  },
+  {
+    "name": "理财",
+    "iconName": "fa-cny",
+    "id": "12",
+    "type": "income"
+  },
+]
+
+export const testItems = [
+  {
+    "title": "buy stuff for kitten",
+    "price": 100,
+    "date": "2021-02-15",
+    "monthCategory": "2021-2",
+    "id": "_kly1klf4g",
+    "cid": "1",
+    "timestamp": 1613347200000
+  },
+  {
+    "title": "这是我的工资",
+    "price": 20000,
+    "date": "2021-02-18",
+    "monthCategory": "2021-2",
+    "id": "_bd16bjeen",
+    "cid": "2",
+    "timestamp": 1613606400000
+  },
+  {
+    "title": "和哥们一起喝酒",
+    "price": 300,
+    "date": "2021-02-20",
+    "monthCategory": "2021-2",
+    "id": "_jjfice21k",
+    "cid": "3",
+    "timestamp": 1613779200000
+  },
+  {
+    "title": "理财收入",
+    "price": 1000,
+    "date": "2021-02-11",
+    "monthCategory": "2021-2",
+    "id": "_1fg1wme63",
+    "cid": "11",
+    "timestamp": 1613001600000
+  },
+  {
+    "title": "理财收入",
+    "price": 300,
+    "date": "2021-01-15",
+    "monthCategory": "2021-1",
+    "id": "_qryggm5y8",
+    "cid": "12",
+    "timestamp": 1610668800000
+  },
+  {
+    "title": "请别人吃饭",
+    "price": 300,
+    "date": "2021-01-15",
+    "monthCategory": "2021-1",
+    "id": "_qryggm511",
+    "cid": "3",
+    "timestamp": 1610668800000
+  }
+]
